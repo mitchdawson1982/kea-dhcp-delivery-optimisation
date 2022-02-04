9fc9f9bd50118a7d31c43ec7c7f71cc2f37360d0
Mitch Dawson <mitchell.dawson@justice.gov.uk>
2022-02-04 10:34:29 +0000

    Wrapped in main function.

--- main.py
+++ main.py
@@ -16,49 +16,46 @@ option_data = [
     }
 ]
 
-# Open the kea dhcp config fie, load the json into a python dictionary and close the input file.
-kea_config_file = open(KEA_DHCP_INPUT_FILE_PATH, 'r')
-kea_data = json.load(kea_config_file)
-kea_config_file.close()
 
-# Open the fits id guid map config fie, load the json into a python dictionary and close the input file.
-fits_id_guid_map_file = open(SITE_ID_GUID_MAP_PATH, 'r')
-fits_id_guid_map_data = json.load(kea_config_file)
-fits_id_guid_map_file.close()
+def main():
+    # Open the kea dhcp config fie, load the json into a python dictionary and close the input file.
+    kea_config_file = open(KEA_DHCP_INPUT_FILE_PATH, 'r')
+    kea_data = json.load(kea_config_file)
+    kea_config_file.close()
 
+    # Open the fits id guid map config fie, load the json into a python dictionary and close the input file.
+    fits_id_guid_map_file = open(SITE_ID_GUID_MAP_PATH, 'r')
+    fits_id_guid_map_data = json.load(fits_id_guid_map_file)
+    fits_id_guid_map_file.close()
 
-# Break out the shared networks element from the kea config.
-shared_nets = kea_data['Dhcp4']['shared-networks']
-
-# Iterate through the shared networks.
-for shared_net in shared_nets:
-    subnets = shared_net['subnet4']
-    if not subnets:
-        continue
-    for subnet in subnets:
-        site_id = subnet['user-context']['site-id']
-        if site_id not in fits_id_guid_map_data:
-            fits_id_guid_map_data[site_id] = str(uuid.uuid4())
-
-        if 'option-data' in subnet:
+    # Break out the shared networks element from the kea config.
+    shared_nets = kea_data['Dhcp4']['shared-networks']
+    # Iterate through the shared networks.
+    for shared_net in shared_nets:
+        subnets = shared_net['subnet4']
+        if not subnets:
             continue
+        for subnet in subnets:
+            site_id = subnet['user-context']['site-id']
+            if site_id not in fits_id_guid_map_data:
+                fits_id_guid_map_data[site_id] = str(uuid.uuid4())
 
-        # option_data[0]['data'] = fits_id_guid_map[site_id]
-        # subnet["option-data"] = option_data
+            if 'option-data' in subnet:
+                continue
+            # Populate the option data field with the guid based on the site id.
+            option_data[0]['data'] = fits_id_guid_map_data[site_id]
+            # Populate the subnet with the option data.
+            subnet["option-data"] = option_data
+
+    # Open the file and Write the site_id_map_json data back to a file.
+    site_id_map_json = open(SITE_ID_GUID_MAP_PATH, 'w')
+    site_id_map_json.write(json.dumps(fits_id_guid_map_data))
+    site_id_map_json.close()
+    # Open the file and Write the updated kea dhcp config to a new file.
+    kea_dhcp_output_file = open(KEA_DHCP_OUTPUT_FILE_PATH, 'w')
+    kea_dhcp_output_file.write(json.dumps(kea_data))
+    kea_dhcp_output_file.close()
 
 
-site_id_map_json = open(SITE_ID_GUID_MAP_PATH, 'w')
-site_id_map_json.write(json.dumps(fits_id_guid_map_data))
-
-# new_json = open('new_kea_dhcp.json', 'w')
-# new_json.write(json.dumps(kea_data))
-# new_json.close()
-
-
-
-
-# pprint(kea_data['Dhcp4']['option-data'])
-
-
-# for subnet in subnets:
-#     pass
+if __name__ == '__main__':
+    main()
